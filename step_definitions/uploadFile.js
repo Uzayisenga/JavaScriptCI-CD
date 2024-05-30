9606f25f3e7bb6060c6bcbda62483bf83e913945
Uzayisenga <aminauzayisenga@gmail.com>
2024-05-30 17:03:56 +0200

    Add github action functionne

--- step_definitions/uploadFile.js
+++ step_definitions/uploadFile.js
@@ -1,107 +1,30 @@
-// const { until,By,Builder,successMessage } = require('selenium-webdriver');
-// const { When, Then, setDefaultTimeout } = require('@cucumber/cucumber');
-// const RegisterPage = require('../PageObjects/RegisterPage');
-// const assert = require('assert');
-// const path = require('path');
 
-// // Set default timeout to 60 seconds
-// setDefaultTimeout(60 * 1000);
-
-
-// let driver; 
-// let registerPage;
-
-// When('a user clicks on selenium link', async function () {
-//     driver = new Builder().forBrowser('firefox').build();
-//     registerPage = new RegisterPage(driver);
-//     await driver.get('https://demo.guru99.com/V1/index.php');
-//     await registerPage.selenium();
-// });
-
-// When('scroll down and clicks on File Upload link', async function () {
-//     await registerPage.fileUpload();
-// });
-
-// When('clicks on choose file', async function () {
-//     await driver.get('https://demo.guru99.com/test/upload/');
-//     const filePath = path.resolve('C:/Users/User/Desktop/Kwibuka-30.jpg'); 
-//     await registerPage.choosedocument(filePath);
-// });
-
-// When('select file from local machine', async function () {
-//     // This step can be handled within 'clicks on choose file' step as the file is already selected from local machine.
-// });
-
-// When('the user clicks on checkbox for I accept terms of service', async function () {
-//     await registerPage.acceptTerms();
-// });
-
-// When('clicks on submit button file', async function () {
-//     await registerPage.submitFile();
-// });
-
-// //const assert = require('assert');
-
-// // Then('the user should receive {string} message', async function (message) {
-// //     try {
-// //         const successMessageElement = await driver.wait(until.elementLocated(By.xpath("//center[contains(text(), 'has been successfully uploaded.')]")), 10000);
-// //         const successMessage = await successMessageElement.getText();
-// //         assert.strictEqual(successMessage.trim(), message.trim());
-// //     } catch (error) {
-// //         console.error('Error finding success message:', error);
-// //         throw error;
-// //     }
-// // });
-
-// Then('the user should receive "has been successfully uploaded" message', async function (expectedMessage) {
-//     try {
-//         // Wait for the element to be located and visible
-//         const successMessageElement = await driver.wait(
-//         until.elementLocated(By.xpath('//*[@id="res"]/center'))
-//         );
-//         await driver.wait(until.elementIsVisible(successMessageElement), 5000);
-
-//         // Get the text content of the element
-//         const successMessage = await successMessageElement.getText();
-
-//         // Debugging output to verify actual message
-//         console.log('Actual success message:', successMessage);
-
-//         // Compare the actual message with the expected message
-//         assert.strictEqual(successMessage == expectedMessage());
-//     } catch (error) {
-//         console.error('Error finding success message:', error);
-
-//         // Print the current page source for debugging purposes
-//         const pageSource = await driver.getPageSource();
-//         console.log('Current page source:', pageSource);
-
-//         // Capture a screenshot for debugging purposes
-//         await driver.takeScreenshot().then((image, err) => {
-//             require('fs').writeFile('error_screenshot.png', image, 'base64', function(err) {
-//                 if (err) console.log(err);
-//             });
-//         });
-//         // let successMessage = await driver.wait(until.elementLocated(By.xpath('//center[contains(text(), "file has been successfully uploaded.")]')), 20000);
-//         // console.log(await successMessage.getText());
-//         throw error;
-//     } finally {
-//         // Quit the driver after the test is done
-//         await driver.quit();
-//     }
-// });
-
-const { until, By, Builder } = require('selenium-webdriver');
-const { When, Then, setDefaultTimeout } = require('@cucumber/cucumber');
-const RegisterPage = require('../PageObjects/RegisterPage');
-const assert = require('assert');
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 const path = require('path');
 
-// Set default timeout to 60 seconds
-setDefaultTimeout(60 * 1000);
+(async function example() {
+  let options = new chrome.Options();
+  let service = new chrome.ServiceBuilder('/usr/local/bin/chromedriver').build();
+  
+  let driver = await new Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(options)
+      .setChromeService(service)
+      .build();
+      const { until, By, Builder } = require('selenium-webdriver');
+      const { When, Then, setDefaultTimeout } = require('@cucumber/cucumber');
+      const RegisterPage = require('../PageObjects/RegisterPage');
+      const assert = require('assert');
+      const path = require('path');
+      
+      // Set default timeout to 60 seconds
+      setDefaultTimeout(60 * 1000);
+      
+      //let driver; 
+      let registerPage;
+  try {
 
-let driver; 
-let registerPage;
 
 When('a user clicks on selenium link', async function () {
     driver = new Builder().forBrowser('chrome').build();
@@ -177,4 +100,7 @@ Then('the user should receive {string} message', async function (expectedMessage
     }
 });
 
-
+} finally {
+    await driver.quit();
+  }
+})();
